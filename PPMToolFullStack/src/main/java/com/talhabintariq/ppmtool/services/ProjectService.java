b9a2c8c7df07c3ef7a1509bd96e9b535c72e845e
Talha <talhabintariq@gmail.com>
2019-10-19 19:52:26 -0400

    Custom Exception Handling

--- PPMToolFullStack/src/main/java/com/talhabintariq/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/talhabintariq/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.talhabintariq.ppmtool.services;
 
 import com.talhabintariq.ppmtool.domain.Project;
+import com.talhabintariq.ppmtool.exceptions.ProjectIdException;
 import com.talhabintariq.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,12 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '"+ project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
+
     }
 }
