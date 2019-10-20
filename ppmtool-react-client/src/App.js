0b71a18cf306890ea3f7cbedc71173659bde6fa9
Talha <talhabintariq@gmail.com>
2019-10-20 01:23:28 -0400

    Added Bootstrap

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Dashboard from './componenets/Dashboard';
 import Header from './componenets/Layout/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
