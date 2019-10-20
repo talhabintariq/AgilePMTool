7ccc584998e5de5c688872d84f949feab7e56933
Talha <talhabintariq@gmail.com>
2019-10-19 20:46:16 -0400

    Find All Projects

--- PPMToolFullStack/src/main/java/com/talhabintariq/ppmtool/repositories/ProjectRepository.java
+++ PPMToolFullStack/src/main/java/com/talhabintariq/ppmtool/repositories/ProjectRepository.java
@@ -9,4 +9,6 @@ public interface ProjectRepository extends CrudRepository<Project, Long> {
 
     Project findByProjectIdentifier(String projectId);
 
+    @Override
+    Iterable<Project> findAll();
 }
