641db6d3cb5de064f5935e77e2e30de45a201a5d
Talha <talhabintariq@gmail.com>
2019-11-10 19:34:01 -0500

    Added Redux

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import './App.css';
-import Dashboard from './componenets/Dashboard';
-import Header from './componenets/Layout/Header';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import AddProject from './componenets/Project/AddProject';
+import React from "react";
+import "./App.css";
+import Dashboard from "./componenets/Dashboard";
+import Header from "./componenets/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./componenets/Project/AddProject";
+import { Provider } from "react-redux";
+import store from './store';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/addProject" component={AddProject} />
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
