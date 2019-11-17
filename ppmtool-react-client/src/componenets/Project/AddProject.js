f2a14e1042794aa960ae7cd1ab02ffe5b337e6ae
Talha <talhabintariq@gmail.com>
2019-11-17 10:25:21 -0500

    errors to store

--- ppmtool-react-client/src/componenets/Project/AddProject.js
+++ ppmtool-react-client/src/componenets/Project/AddProject.js
@@ -12,11 +12,18 @@ class AddProject extends Component {
       projectIdentifier: "",
       description: "",
       start_date: "",
-      end_date: ""
+      end_date: "",
+      errors: {},
     };
 
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
   }
 
   onChange(e) {
@@ -37,19 +44,11 @@ class AddProject extends Component {
   }
 
   render() {
+    const { errors } = this.state;
+    console.log(errors);
+
     return (
       <div>
-        {
-          //check name attribute input fields
-          //create constructor
-          //set state
-          //set value on input fields
-          //create onChange function
-          //set onChange on each input field
-          //bind on constructor
-          //check state change in the react extension
-        }
-
         <div className="project">
           <div className="container">
             <div className="row">
@@ -66,6 +65,7 @@ class AddProject extends Component {
                       value={this.state.projectName}
                       onChange={this.onChange}
                     />
+                    <span>{ errors.projectName }</span>
                   </div>
                   <div className="form-group">
                     <input
@@ -76,6 +76,7 @@ class AddProject extends Component {
                       value={this.state.projectIdentifier}
                       onChange={this.onChange}
                     />
+                    <span>{ errors.projectIdentifier }</span>
                   </div>
                   <div className="form-group">
                     <textarea
@@ -85,6 +86,7 @@ class AddProject extends Component {
                       value={this.state.description}
                       onChange={this.onChange}
                     />
+                    <span>{ errors.description }</span>
                   </div>
                   <h6>Start Date</h6>
                   <div className="form-group">
@@ -122,7 +124,12 @@ class AddProject extends Component {
 }
 
 AddProject.propTypes = {
-  createProject: PropTypes.func.isRequired
+  createProject: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired,
 }
 
-export default connect(null, { createProject })(AddProject);
+const mapStateToProps = state => ({
+  errors: state.errors,
+});
+
+export default connect(mapStateToProps, { createProject })(AddProject);
