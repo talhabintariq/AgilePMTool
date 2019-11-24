cea56081ae9ccfdba2f00f4abe2e66405a9c7850
Talha <talhabintariq@gmail.com>
2019-11-24 13:51:42 -0500

    validation

--- ppmtool-react-client/src/componenets/Project/UpdateProject.js
+++ ppmtool-react-client/src/componenets/Project/UpdateProject.js
@@ -1,46 +1,31 @@
 import React, { Component } from "react";
-import { getProject, createProject } from '../../actions/projectActions';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import classnames from 'classnames';
+import { getProject, createProject } from "../../actions/projectActions";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classnames from "classnames";
 
 class UpdateProject extends Component {
+  //set state
   constructor() {
     super();
 
     this.state = {
-      "id": "",
+      id: "",
       projectName: "",
       projectIdentifier: "",
       description: "",
       start_date: "",
       end_date: "",
+      errors: {}
     };
-  }
-
-  componentDidMount() {
-    const { id } = this.props.match.params;
-    this.props.getProject(id, this.props.history);
-  }
-
-  onChange = (e) => {
-    this.setState({[e.target.name]: e.target.value })
-  }
-
-  onSubmit = (e) => {
-    e.preventDefault();
-    const updateProject = {
-      id: this.state.id,
-      projectName: this.state.projectName,
-      projectIdentifier: this.state.projectIdentifier,
-      description: this.state.description,
-      start_date: this.state.start_date,
-      end_date: this.state.end_date
-    };
-    this.props.createProject(updateProject, this.props.history);
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
     const {
       id,
       projectName,
@@ -60,7 +45,32 @@ class UpdateProject extends Component {
     });
   }
 
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.getProject(id, this.props.history);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const updateProject = {
+      id: this.state.id,
+      projectName: this.state.projectName,
+      projectIdentifier: this.state.projectIdentifier,
+      description: this.state.description,
+      start_date: this.state.start_date,
+      end_date: this.state.end_date
+    };
+
+    this.props.createProject(updateProject, this.props.history);
+  }
+
   render() {
+    const { errors } = this.state;
     return (
       <div className="project">
         <div className="container">
@@ -72,12 +82,17 @@ class UpdateProject extends Component {
                 <div className="form-group">
                   <input
                     type="text"
-                    className="form-control form-control-lg "
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.projectName
+                    })}
                     placeholder="Project Name"
                     name="projectName"
                     value={this.state.projectName}
                     onChange={this.onChange}
                   />
+                  {errors.projectName && (
+                    <div className="invalid-feedback">{errors.projectName}</div>
+                  )}
                 </div>
                 <div className="form-group">
                   <input
@@ -92,12 +107,17 @@ class UpdateProject extends Component {
                 </div>
                 <div className="form-group">
                   <textarea
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.description
+                    })}
                     placeholder="Project Description"
-                    value={this.state.description}
                     name="description"
                     onChange={this.onChange}
+                    value={this.state.description}
                   />
+                  {errors.description && (
+                    <div className="invalid-feedback">{errors.description}</div>
+                  )}
                 </div>
                 <h6>Start Date</h6>
                 <div className="form-group">
@@ -123,7 +143,6 @@ class UpdateProject extends Component {
                 <input
                   type="submit"
                   className="btn btn-primary btn-block mt-4"
-                  name="submit"
                 />
               </form>
             </div>
@@ -136,14 +155,17 @@ class UpdateProject extends Component {
 
 UpdateProject.propTypes = {
   getProject: PropTypes.func.isRequired,
-  project: PropTypes.object.isRequired,
   createProject: PropTypes.func.isRequired,
-}
+  project: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
 
 const mapStateToProps = state => ({
   project: state.project.project,
-})
+  errors: state.errors
+});
 
-export default connect(mapStateToProps,
+export default connect(
+  mapStateToProps,
   { getProject, createProject }
 )(UpdateProject);
